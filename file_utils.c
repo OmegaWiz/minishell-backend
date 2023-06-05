ce227f2a4279dfda210db4be95b5edb1901c8b07
MoonEgg <kraiwish123@gmail.com>
2023-06-05 11:53:41 +0700

    refactor: :art: move file_close() to a new file

--- file_utils.c
+++ file_utils.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fs_utils.c                                         :+:      :+:    :+:   */
+/*   file_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/30 08:36:36 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2023/06/05 11:50:04 by kkaiyawo         ###   ########.fr       */
+/*   Created: 2023/06/05 11:50:14 by kkaiyawo          #+#    #+#             */
+/*   Updated: 2023/06/05 11:50:25 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "backend.h"
 
-t_fileset	*fs_init(char *name, int fd, t_filetype type)
+void	file_close(int *fd)
 {
-	t_fileset	*fs;
-
-	fs = malloc(sizeof(t_fileset));
-	if (!fs)
-		return (NULL);
-	fs->name = name;
-	fs->fd = fd;
-	fs->type = type;
-	return (fs);
-}
-
-void	fs_free(t_fileset *fs)
-{
-	if (!fs)
-		return ;
-	if (fs->fd > 2)
-		file_close(&fs->fd);
-	if (fs->name)
-		free(fs->name);
-	free(fs);
+	if (*fd > 2)
+		close(*fd);
+	*fd = INT_MIN;
 }
